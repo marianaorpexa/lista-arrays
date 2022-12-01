12730aabc37088855e83ae878cc7b2c36dd2c9f0
Mariana Rodrigues <rodriguesmariana1106@gmail.com>
2022-12-01 17:20:04 -0300

    exercicio resolvido 1

---
+++ exercicio-resolvido-array-1.js
@@ -0,0 +1,34 @@
+// criar uma lista com 5 paises
+
+const paises = ['Brasil', 'Argentina', 'Espanha', 'Chile'];
+console.log(paises);
+
+// remova um paises do fim da lista
+
+paises.pop();
+console.log(paises);
+
+// Aadd 1 pais ao inicio da lista
+
+paises.unshift('EUA');
+console.log(paises);
+
+
+// Remover 1 pais do inicio da lista
+
+paises.shift();
+console.log(paises);
+
+// imprimir o ultimo pais na tela
+const ultimo = paises[paises.length - 1];
+//console.log(ultimo);
+console.log(paises[paises.length - 1]);
+
+// imprima o segunda pais da lista
+console.log(paises[1]);
+
+
+// imprima o pais de indice 2
+console.log(paises[2]);
+
+
